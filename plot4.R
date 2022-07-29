7a1d2bf9c1b8cbdc4f240c7ca1fcb08738ba6aef
amethystbian <108068625+amethystbian@users.noreply.github.com>
2022-07-29 19:56:19 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -24,3 +24,5 @@ lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+while (!is.null(dev.list()))  dev.off()
